885d04406819517bf8422f8ffba1b71be60b1505
İrem Özturk <iremozturk_@outlook.com>
2025-07-22 11:03:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/optimus-world-time/', // 👈 BURASI ÖNEMLİ
+  base: '/optimus-world-time/',
 })
