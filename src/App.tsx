429d5baa37d65543705598f2884289a96cc0a4ae
iremmozturkk <iremozturk_@outlook.com>
2025-08-07 09:37:54 +0300

    Instead of showing the SplashScreen for a fixed 1-second delay, we now show it until the getTimezones API call finishes.
    We use useQuery to fetch the timezones via React Query.
    As long as isLoading is true, the user sees the SplashScreen.
    So, the splash screen now waits for actual data, not just a delay animation.
    To prevent the app from rendering before the necessary data is loaded, ensuring a smoother and safer user experience.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CityDetail from "./pages/CityDetail";
-import { ConfigProvider } from "./contexts/configProvider";
 import SplashScreen from "./pages/SplashScreen";
-import { useEffect, useState } from "react";
+import { ConfigProvider } from "./contexts/configProvider";
 import { useQuery } from "@tanstack/react-query";
 import { getTimezones } from "./api/timeApi";
 
 export default function App() {
-  const [showSplash, setShowSplash] = useState(true);
-
+  
+  // API'den timezone verisini al
   const { isLoading } = useQuery({
     queryKey: ["timezones"],
     queryFn: getTimezones,
-    select: (res)=>  res[0],
-    staleTime: Infinity
+    staleTime: Infinity, // Tek sefer çek, sürekli fetch etmesin
   });
 
-  useEffect(() => {
-    const timer = setTimeout(() => setShowSplash(false), 1000); 
-    return () => clearTimeout(timer);
-  }, []);
-
-  const stillLoading = showSplash || isLoading; //Veri yüklenene kadar splashScreen kalsın
+  // SplashScreen veri yüklenene kadar gösterilsin
+  if (isLoading) { return <SplashScreen />;}
 
   return (
     <ConfigProvider>
-      {stillLoading ? (
-        <SplashScreen />
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/city/:zone" element={<CityDetail />} />
-          <Route path="*" element={<div>404 - Sayfa Bulunamadı</div>} />
-        </Routes>
-      )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/city/:zone" element={<CityDetail />} />
+      </Routes>
     </ConfigProvider>
   );
 }
